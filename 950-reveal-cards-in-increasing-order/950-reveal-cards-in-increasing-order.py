a9cac43d4defdc8ccf9afda41ed15b149a9f29dd
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-02-24 15:46:40 -0700

    Time: 78 ms (26.67%), Space: 14.1 MB (94.62%) - LeetHub

---
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort()
+        res = [deck.pop()]
+        for i in range(1, len(deck) + 1):
+            res = [deck.pop()] + [res.pop()] + res
+            
+        return res
+                    
+        
