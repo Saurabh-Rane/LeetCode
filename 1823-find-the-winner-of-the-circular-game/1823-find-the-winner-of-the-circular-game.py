a5f8e4bd6e3cef08c51b9b8b82733097fba2e94f
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-02-24 18:50:35 -0700

    Time: 376 ms (22.25%), Space: 13.8 MB (99.68%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,14 +1,11 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        queue = collections.deque()
-        
-        for i in range(1, n + 1):
-            queue.appendleft(i)
+        queue = collections.deque([i for i in range(1, n + 1)])
         
         while len(queue) != 1:
-            for i in range(1, k):
-                queue.appendleft(queue[-1])
-                queue.pop()
-            queue.pop()
+            for i in range(k - 1):
+                queue.append(queue.popleft())
+            queue.popleft()
             
         return queue.pop()
+    
