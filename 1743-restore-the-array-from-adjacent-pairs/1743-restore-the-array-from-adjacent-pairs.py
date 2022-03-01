f6df2e27b3d98980410c6647fa5a2905f5a2b7af
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-02-28 18:47:44 -0700

    Time: 1233 ms (81.88%), Space: 62.6 MB (91.16%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        hashmap = collections.defaultdict(list)
+        res = []
+        seen = set()
+        
+        for i, j in adjacentPairs:
+            hashmap[i].append(j)
+            hashmap[j].append(i)
+            
+        curr = None
+        for i in hashmap:
+            if len(hashmap[i]) == 1:
+                curr = i
+                break
+        
+        while curr != None:
+            res.append(curr)
+            seen.add(curr)
+            adj = hashmap[curr]
+            curr = None
+            
+            for i in adj:
+                if i not in seen:
+                    curr = i
+        
+        return res
