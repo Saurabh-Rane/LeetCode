fdf7e0230fc920a0b3549928f56341974eb8d6ee
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-03-02 01:12:37 -0700

    Time: 67 ms (6.62%), Space: 14 MB (79.38%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -1,23 +1,25 @@
 class HitCounter:
 
     def __init__(self):
-        self.time = [0] * 300
+        self.index = [0] * 300
         self.count = [0] * 300
 
     def hit(self, timestamp: int) -> None:
-        index = timestamp % 300
-        if self.time[index] != timestamp:
-            self.time[index] = timestamp
-            self.count[index] = 1
+        val = timestamp % 300
+        if self.index[val] != timestamp:
+            self.index[val] = timestamp
+            self.count[val] = 1
         else:
-            self.count[index] += 1
+            self.count[val] += 1
+        
 
     def getHits(self, timestamp: int) -> int:
         res = 0
         for i in range(300):
-            if timestamp >= self.time[i] > timestamp - 300:
+            if timestamp >= self.index[i] > timestamp - 300:
                 res += self.count[i]
         return res
+        
 
 
 # Your HitCounter object will be instantiated and called as such:
