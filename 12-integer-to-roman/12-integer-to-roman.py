e1a5fdaef6f5c3cbc4c4456dfab8eff385f4f218
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-04-01 21:31:33 -0700

    Time: 52 ms (87.34%), Space: 14 MB (7.40%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -4,7 +4,7 @@ class Solution:
                      ['XL', 40], ['L', 50], ['XC', 90], ['C', 100], ['CD', 400],
                     ['D', 500], ['CM', 900], ['M', 1000]]
         res = ''
-        for sym, i in romanList[-1::-1]:
+        for sym, i in romanList[::-1]:
             if num // i:
                 count = num // i
                 res += sym * count
