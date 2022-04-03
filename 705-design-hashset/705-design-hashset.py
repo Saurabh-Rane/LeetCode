7e615c5abea51b5ba08a7c013cec5c05b99a7fd8
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-04-03 12:22:23 -0700

    Time: 325 ms (47.37%), Space: 19.5 MB (44.77%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,18 +1,27 @@
 class MyHashSet:
 
     def __init__(self):
-        self.list = [False] * 1000001
+        self.size = 10000
+        self.bucket = [[] for i in range(self.size)]
+    
+    def hashing(self, key):
+        return key % self.size
 
     def add(self, key: int) -> None:
-        if not self.list[key]:
-            self.list[key] = True
+        index = self.hashing(key)
+        if key not in self.bucket[index]:
+            self.bucket[index].append(key)
 
     def remove(self, key: int) -> None:
-        if self.list[key]:
-            self.list[key] = False
+        index = self.hashing(key)
+        if key in self.bucket[index]:
+            self.bucket[index].remove(key)
 
     def contains(self, key: int) -> bool:
-        return self.list[key]
+        index = self.hashing(key)
+        if key in self.bucket[index]:
+            return True
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
