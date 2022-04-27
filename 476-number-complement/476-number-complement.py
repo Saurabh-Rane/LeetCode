4ced78635aad5e43c3114ebc0c9bbb64384b38dc
Saurabh <79814880+Saurabh-Rane@users.noreply.github.com>
2022-04-26 21:48:57 -0700

    Time: 45 ms (40.13%), Space: 14 MB (9.87%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -1,20 +1,12 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        res = 0
         
-        i = 0
-        while num:
-            rem = 0 if num % 2 else 1
-            res = (rem * (10 ** i)) + res
-            num = num // 2
-            i += 1
-        #print(res)
+        exor = 0
+        temp = num
         
-        i = 0
-        while res:
-            rem = res % 10
-            num += rem * (2 ** i)
-            i += 1
-            res = res // 10
-            
-        return num
+        while temp:
+            exor = exor << 1
+            exor ^= 1
+            temp >>= 1
+        
+        return exor ^ num
